06c50884cb07284e3aa81dd33bfabaaa55a65232
Asmi4 <suman.asmita0408@gmail.com>
2020-04-05 20:35:02 +0530

    Jobs app up and running

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length= 200)
